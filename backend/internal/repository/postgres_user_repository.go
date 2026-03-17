b55fc0d19a020c7045e3014177e154488b50d518
Masega360 <tomicaporaso@gmail.com>
2026-03-17 11:47:36 -0300

    Refactor PostgresUserRepository to remove function signatures and implement Create method

--- backend/internal/repository/postgres_user_repository.go
+++ backend/internal/repository/postgres_user_repository.go
@@ -1,6 +1,24 @@
+package backend.internal.repository
+
 type PostgresUserRepository struct {
-	Create  func(user *User) error
-	GetByID func(id string) (*User, error)
-	Update  func(user *User) error
-	Delete  func(user *User) error
+}
+
+func (r *PostgresUserRepository) Create(user User) error {
+	// Implementation for creating a user in PostgreSQL
+	return nil
+}
+
+func (r *PostgresUserRepository) GetByID(id UUID) (User, error) {
+	// Implementation for getting a user by ID from PostgreSQL
+	return User{}, nil
+}
+
+func (r *PostgresUserRepository) Update(id UUID, user User) error {
+	// Implementation for updating a user in PostgreSQL
+	return nil
+}
+
+func (r *PostgresUserRepository) Delete(id UUID) error {
+	// Implementation for deleting a user from PostgreSQL
+	return nil
 }
