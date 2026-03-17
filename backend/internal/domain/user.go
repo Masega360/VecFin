f312f513405f64a7188b4b6ea6f37623ecafae4e
MRecioO <matiasrecioojeda@gmail.com>
2026-03-17 10:52:26 -0300

    user.go

---
+++ backend/internal/domain/user.go
@@ -0,0 +1,16 @@
+package domain
+
+import (
+	
+)
+
+type User struct {
+	id UUID
+	first_name string
+	last_name string
+	email string
+	password_hash string
+	risk_profile riskType
+	registration_date timestamp
+	last_access timestamp
+}
