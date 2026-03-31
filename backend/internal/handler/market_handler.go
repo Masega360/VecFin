04babbd2c648ff3e7c2a246f2cc9a4ca3a691815
Masega <tomicaporaso@gmail.com>
2026-03-31 12:44:14 -0300

    refactor: update MarketHandler to use domain.MarketService and improve SearchAssets method

--- backend/internal/handler/market_handler.go
+++ backend/internal/handler/market_handler.go
@@ -1,5 +1,33 @@
 package handler
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Masega360/vecfin/backend/internal/domain"
+)
+
 type MarketHandler struct {
-	uc MarketService
+	uc domain.MarketService
+}
+
+func NewMarketHandler(uc domain.MarketService) *MarketHandler {
+	return &MarketHandler{uc: uc}
+}
+
+func (h *MarketHandler) SearchAssets(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("query")
+	if query == "" {
+		http.Error(w, "Query parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	assets, err := h.uc.SearchAssets(query)
+	if err != nil {
+		http.Error(w, "Error searching assets: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(assets)
 }
